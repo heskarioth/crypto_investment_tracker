fc829d4e7c9ecfa4e64b4d28bd569136be6fa4c7
Camillo Heska <camilloshady@gmail.com>
2022-02-06 15:51:43 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#from distutils.core import setup
+import setuptools
+
+setuptools.setup(
+    name='crypto_investment_tracker',
+    version='1.0.0',
+    packages=['crypto_investment_tracker'],
+    license='MIT',
+    description = 'One In All Solution for your Crypto Investing Tracking',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author = 'Camillo Heska',
+    install_requires=['gspread','binance-connector'],
+    url = 'https://github.com/heskarioth/crypto_investment_tracker',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    )
